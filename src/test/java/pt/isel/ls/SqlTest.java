2d060541caff0d458201273e35a9a44e1ea0f30e
PTTANK <nuno_faiao@hotmail.com>
2018-03-01 14:11:58 +0000

    try finally

--- src/test/java/pt/isel/ls/SqlTest.java
+++ src/test/java/pt/isel/ls/SqlTest.java
@@ -13,71 +13,85 @@ import static org.junit.Assert.assertTrue;
 public class SqlTest {
 
     @Test
-    public void insert_students() throws SQLException{
-        Connection con = Sql.CreateConnetion();
-        con.setAutoCommit(false);
-        int number = 10;
-        String name = "NomeTeste";
-        ResultSet rs;
-        PreparedStatement stmt = con.prepareStatement("insert into students values(?, ?)");
-        stmt.setString(1, name);
-        stmt.setInt(2, number);
-        stmt.execute();
+    public void insert_students() throws SQLException {
+        Connection con = null;
+        try {
+            con = Sql.CreateConnetion();
+            con.setAutoCommit(false);
+            int number = 10;
+            String name = "NomeTeste";
+            ResultSet rs;
+            PreparedStatement stmt = con.prepareStatement("insert into students values(?, ?)");
+            stmt.setString(1, name);
+            stmt.setInt(2, number);
+            stmt.execute();
 
-        PreparedStatement stmt2 = con.prepareStatement("select number from students where name = ?");
-        stmt2.setString(1, name);
-        rs = stmt2.executeQuery();
-        int n = 0;
-        if(rs.next()) n = rs.getInt(1);
-        assertEquals(n, number);
+            PreparedStatement stmt2 = con.prepareStatement("select number from students where name = ?");
+            stmt2.setString(1, name);
+            rs = stmt2.executeQuery();
+            int n = 0;
+            if (rs.next()) n = rs.getInt(1);
+            assertEquals(n, number);
+        } finally {
+            con.rollback();
+            con.close();
+        }
 
-        con.rollback();
-        con.close();
+
     }
 
     @Test
     public void update_students() throws SQLException{
-        Connection con = Sql.CreateConnetion();
-        con.setAutoCommit(false);
-        int number = 10;
-        String name = "NomeTeste";
-        ResultSet rs;
-        PreparedStatement stmt = con.prepareStatement("insert into students values(?, ?)");
-        stmt.setString(1, name);
-        stmt.setInt(2, number);
-        stmt.execute();
+        Connection con = null;
+        try{
+            con = Sql.CreateConnetion();
+            con.setAutoCommit(false);
+            int number = 10;
+            String name = "NomeTeste";
+            ResultSet rs;
+            PreparedStatement stmt = con.prepareStatement("insert into students values(?, ?)");
+            stmt.setString(1, name);
+            stmt.setInt(2, number);
+            stmt.execute();
 
-        PreparedStatement stmt2 = con.prepareStatement("update students set name = 'NewName' where number = ?");
-        stmt2.setInt(1, number);
-        stmt2.execute();
+            PreparedStatement stmt2 = con.prepareStatement("update students set name = 'NewName' where number = ?");
+            stmt2.setInt(1, number);
+            stmt2.execute();
 
-        PreparedStatement stmt3 = con.prepareStatement("select name from students where number = ?");
-        stmt3.setInt(1, number);
-        rs = stmt3.executeQuery();
-        String res = "";
-        if(rs.next()) res = rs.getString(1);
-        assertEquals("NewName", res);
-
-        con.rollback();
-        con.close();
+            PreparedStatement stmt3 = con.prepareStatement("select name from students where number = ?");
+            stmt3.setInt(1, number);
+            rs = stmt3.executeQuery();
+            String res = "";
+            if(rs.next()) res = rs.getString(1);
+            assertEquals("NewName", res);
+        }finally {
+            con.rollback();
+            con.close();
+        }
     }
 
     @Test
-    public void delete_student() throws SQLException{
-        Connection con = Sql.CreateConnetion();
-        con.setAutoCommit(false);
-        int number = 10;
-        String name = "NomeTeste";
-        ResultSet rs;
-        PreparedStatement stmt = con.prepareStatement("insert into students values(?, ?)");
-        stmt.setString(1, name);
-        stmt.setInt(2, number);
-        stmt.execute();
+    public void delete_student() throws SQLException {
+        Connection con = null;
+        try {
+            con = Sql.CreateConnetion();
+            con.setAutoCommit(false);
+            int number = 10;
+            String name = "NomeTeste";
+            ResultSet rs;
+            PreparedStatement stmt = con.prepareStatement("insert into students values(?, ?)");
+            stmt.setString(1, name);
+            stmt.setInt(2, number);
+            stmt.execute();
 
-        PreparedStatement stmt2 = con.prepareStatement("delete from students where number = ?");
-        stmt2.setInt(1, number);
-        int x = stmt2.executeUpdate();
-        assertTrue(x > 0);
-        con.close();
+            PreparedStatement stmt2 = con.prepareStatement("delete from students where number = ?");
+            stmt2.setInt(1, number);
+            int x = stmt2.executeUpdate();
+            assertTrue(x > 0);
+        } finally {
+            con.close();
+        }
+
+
     }
 }
