c65727cf113e6ab6967f895bac3243e53ac9c670
Twigz57 <41457@alunos.isel.ipl.pt>
2018-04-10 13:38:52 +0100

    getTickets, getTicketID and their views

--- src/main/java/pt/isel/ls/core/commands/GetCinemaIDTheaterIDSessionIDTickets.java
+++ src/main/java/pt/isel/ls/core/commands/GetCinemaIDTheaterIDSessionIDTickets.java
@@ -2,12 +2,14 @@ package pt.isel.ls.core.commands;
 
 import pt.isel.ls.core.utils.CommandBuilder;
 import pt.isel.ls.core.utils.DataContainer;
+import pt.isel.ls.model.Movie;
+import pt.isel.ls.model.Session;
+import pt.isel.ls.model.Theater;
+import pt.isel.ls.model.Ticket;
 import pt.isel.ls.view.command.CommandView;
 import pt.isel.ls.view.command.GetCinemaIDTheaterIDSessionIDTicketsView;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 import static pt.isel.ls.core.strings.CommandEnum.CINEMA_ID;
 import static pt.isel.ls.core.strings.CommandEnum.THEATER_ID;
@@ -21,19 +23,43 @@ public class GetCinemaIDTheaterIDSessionIDTickets extends Command {
         /*
             GET /cinemas/{cid}/theaters/{tid}/sessions/{sid}/tickets - returns a list with all tickets for a session.
         */
+        PreparedStatement stmt = connection.prepareStatement(
+                "SELECT tk.seat, tk.row,s.sid, s.Date,m.mid,t.tid,t.SeatsAvailable,t.Rows, t.Seats, t.Theater_Name,c.cid, m.Title, m.Release_Year ,m.Duration " +
+                        "FROM TICKET AS tk " +
+                        "INNER JOIN CINEMA_SESSION AS s ON tk.sid=s.sid " +
+                        "INNER JOIN THEATER AS t ON s.tid=t.tid " +
+                        "INNER JOIN CINEMA AS c ON t.cid=c.cid " +
+                        "INNER JOIN MOVIE AS t ON m.mid=s.mid"
+        );
+        ResultSet rs = stmt.executeQuery();
 
-        ResultSet rs = null; //= stmt.executeQuery();
+        DataContainer data=new DataContainer(cmdBuilder.getHeader());
+        int seat, sid=0, mid, tid, availableSeats, rows, seatsRow, cid, year, duration;
+        String row;
+        Date date;
+        String theaterName, title;
 
-        DataContainer data = new DataContainer(cmdBuilder.getHeader());
         while(rs.next()){
-            //TODO: something like GetCinemaIDSessions
+            seat = rs.getInt(1);
+            row = rs.getString(2);
+            sid = rs.getInt(3);
+            date = rs.getDate(4);
+            mid = rs.getInt(5);
+            tid = rs.getInt(6);
+            availableSeats = rs.getInt(7);
+            rows = rs.getInt(8);
+            seatsRow = rs.getInt(9);
+            theaterName = rs.getString(10);
+            cid = rs.getInt(11);
+            title = rs.getString(12);
+            year = rs.getInt(13);
+            duration = rs.getInt(14);
+
+            data.add(new Ticket(row.charAt(0),seat,new Session(sid, date, new Movie(mid, title, year, duration),
+                    new Theater(tid, theaterName, rows, seatsRow, availableSeats, cid), cid))
+            );
         }
 
-        return new GetCinemaIDTheaterIDSessionIDTicketsView(
-                data,
-                Integer.parseInt(cmdBuilder.getId(String.valueOf(CINEMA_ID))),
-                Integer.parseInt(cmdBuilder.getId(String.valueOf(THEATER_ID))),
-                Integer.parseInt(cmdBuilder.getId(String.valueOf(THEATER_ID)))
-        );
+        return new GetCinemaIDTheaterIDSessionIDTicketsView(data,sid);
     }
 }
--- src/main/java/pt/isel/ls/view/command/GetCinemaIDTheaterIDSessionIDTicketsView.java
+++ src/main/java/pt/isel/ls/view/command/GetCinemaIDTheaterIDSessionIDTicketsView.java
@@ -1,20 +1,45 @@
 package pt.isel.ls.view.command;
 
+import pt.isel.ls.core.headers.Header;
 import pt.isel.ls.core.utils.DataContainer;
+import pt.isel.ls.model.Ticket;
 
 
 public class GetCinemaIDTheaterIDSessionIDTicketsView extends CommandView {
-    private int cinemaId, theaterId, sessionId;
+    private int sessionId;
 
-    public GetCinemaIDTheaterIDSessionIDTicketsView(DataContainer data, int cinemaId, int theaterId, int sessionId) {
+    public GetCinemaIDTheaterIDSessionIDTicketsView(DataContainer data ,int sessionId ) {
         this.data = data;
-        this.cinemaId = cinemaId;
-        this.theaterId = theaterId;
-        this.sessionId = sessionId;
+        this.sessionId=sessionId;
     }
 
     @Override
     public void printAllInfo() {
+        Header header = data.getHeader();
+
+        if (header != null) {
+            header.addTitle("Tickets (CinemaID: "+sessionId+")");
+
+            String[][] tableData  = new String[data.size()][6];
+            String[] tableColumns = {"ID", "Date", "Title", "Duration", "Theater name", "CinemaID"};
+
+            Ticket ticket;
+            for (int y=0; y<data.size(); ++y) {
+                ticket = (Ticket) data.getData(y);
+                tableData[y][0] = String.valueOf(ticket.getId());
+                tableData[y][1] = ticket.getSession().getDate().toString();
+                tableData[y][2] = String.valueOf(ticket.getSession().getMovie().getTitle());
+                tableData[y][3] = String.valueOf(ticket.getSession().getMovie().getDuration());
+                tableData[y][4] = String.valueOf(ticket.getSession().getTheater().getName());
+                tableData[y][5] = String.valueOf(ticket.getSession().getCinemaID());
+            }
+            header.addTable(tableColumns, tableData);
+
+            header.close();
+            header.writeToFile();
+
+            System.out.println(header.getBuildedString());
+        }
         System.out.println("TODO: implement");
     }
 }
