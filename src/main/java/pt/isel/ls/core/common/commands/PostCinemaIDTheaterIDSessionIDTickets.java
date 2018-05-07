ad4c43c70b8d154ea1749393fc3da232b341653d
Twigz57 <41457@alunos.isel.ipl.pt>
2018-05-07 14:39:51 +0100

    small fix on tickets post

--- src/main/java/pt/isel/ls/core/common/commands/PostCinemaIDTheaterIDSessionIDTickets.java
+++ src/main/java/pt/isel/ls/core/common/commands/PostCinemaIDTheaterIDSessionIDTickets.java
@@ -7,6 +7,7 @@ import pt.isel.ls.view.command.PostView;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static pt.isel.ls.core.strings.CommandEnum.*;
@@ -29,22 +30,32 @@ public class PostCinemaIDTheaterIDSessionIDTickets extends Command {
 
     @Override
     public CommandView execute(CommandBuilder cmdBuilder, Connection connection) throws CommandException, SQLException {
-        PreparedStatement stmt = connection.prepareStatement("INSERT INTO TICKET VALUES(?, ?, ?, ?)");
-        String id = cmdBuilder.getParameter(ROWS.toString()) + cmdBuilder.getParameter(SEATS_ROW.toString());
-        stmt.setString(1, id);
-        stmt.setString(2, cmdBuilder.getParameter(SEATS_ROW.toString()));
-        stmt.setString(3, cmdBuilder.getParameter(ROWS.toString()));
-        stmt.setString(4, cmdBuilder.getId(SESSION_ID.toString()));
-        stmt.executeUpdate();
-
-        stmt = connection.prepareStatement("update SEATS SET SEATS.seats = SEATS.seats - 1 from SEATS inner join THEATER on THEATER.tid = Seats.tid " +
-                                            "inner join CINEMA_SESSION on CINEMA_SESSION.sid = SEATS.sid " +
-                                            "WHERE THEATER.tid = ? and CINEMA_SESSION.sid = ?"
+        PreparedStatement stmt=connection.prepareStatement("SELECT s.seats " +
+                "FROM SEATS AS s INNER JOIN CINEMA_SESSION AS cs ON cs.sid=s.sid " +
+                "WHERE s.sid=?"
         );
-        stmt.setString(1, cmdBuilder.getId(THEATER_ID.toString()));
-        stmt.setString(2, cmdBuilder.getId(SESSION_ID.toString()));
-        stmt.executeUpdate();
-        return new PostView<>("Ticket ID: ", id);
+        stmt.setString(1, cmdBuilder.getId(SESSION_ID.toString()));
+        ResultSet rs = stmt.executeQuery();
+        int availableSeats=0;
+        if(rs.next()) availableSeats = rs.getInt(1);
+        if(availableSeats>0) {
+            stmt = connection.prepareStatement("INSERT INTO TICKET VALUES(?, ?, ?, ?)");
+            String id = cmdBuilder.getParameter(ROWS.toString()) + cmdBuilder.getParameter(SEATS_ROW.toString());
+            stmt.setString(1, id);
+            stmt.setString(2, cmdBuilder.getParameter(SEATS_ROW.toString()));
+            stmt.setString(3, cmdBuilder.getParameter(ROWS.toString()));
+            stmt.setString(4, cmdBuilder.getId(SESSION_ID.toString()));
+            stmt.executeUpdate();
+
+            stmt = connection.prepareStatement("UPDATE SEATS SET SEATS.seats = SEATS.seats - 1 "+
+                    "WHERE SEATS.tid = ? AND SEATS.sid = ?"
+            );
+            stmt.setString(1, cmdBuilder.getId(THEATER_ID.toString()));
+            stmt.setString(2, cmdBuilder.getId(SESSION_ID.toString()));
+            stmt.executeUpdate();
+            return new PostView<>("Ticket ID: ", id);
+        }
+        return new PostView<>("Ticket ", "NOT POSTED!");
     }
 
     @Override
