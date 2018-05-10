f03f568bb9b4453e9879c8bf6e79854420fb12b2
PTTANK <nuno_faiao@hotmail.com>
2018-05-11 00:19:05 +0100

    Esboço dos GetCinemaID alterado para as novas funcionalidades. A parte do view tá estranha mas é só para mostrar como exemplo. Leiam os comentários do GetCinemaID

--- src/main/java/pt/isel/ls/view/command/GetCinemaIDView.java
+++ src/main/java/pt/isel/ls/view/command/GetCinemaIDView.java
@@ -3,19 +3,50 @@ package pt.isel.ls.view.command;
 import pt.isel.ls.core.common.headers.Header;
 import pt.isel.ls.core.utils.DataContainer;
 import pt.isel.ls.model.Cinema;
+import pt.isel.ls.model.Theater;
 
 public class GetCinemaIDView extends CommandView {
+    private String theaterName;
+    private String movieTitle;
+    private  String date;
+    private int seatsAv;
+    private int movieDur;
+    private int movRelease;
+    private int theaterCount;
 
-    public GetCinemaIDView(DataContainer data) {
+    public GetCinemaIDView(DataContainer data, int theaterCount) {
         this.data = data;
+        this.theaterCount = theaterCount;
     }
 
     @Override
     protected void allInfo() {
+        Theater theater = (Theater) data.getData(1);
         Header header = data.getHeader();
 
         if (header != null) {
-            Cinema cinema = (Cinema) data.getData(0);
+        Cinema cinema = (Cinema) data.getData(0);
+        header.addObject("Cinema "+cinema.getId(),
+                new String[]{"Name", "City"},
+                new String[]{cinema.getName(), cinema.getCity()}
+        );
+        System.out.println("");
+        header.addTitle("Theaters for Cinema " + cinema.getName() + ": (Theater Name: "+theater.getName()+")");
+
+            String[] tableColumns = {"Theater Name"};
+            String[][] tableData  = new String[theaterCount][tableColumns.length];
+
+            for (int y=0; y<tableData.length; ++y) {
+                tableData[y][0] = theater.getName();
+            }
+            header.addTable("Theater - Name:"+theater.getName(), tableColumns, tableData);
+            infoString = header.getBuildedString();
+
+
+
+
+
+            /*Cinema cinema = (Cinema) data.getData(0);
             header.addObject("Cinema "+cinema.getId(),
                     new String[]{"Name", "City"},
                     new String[]{cinema.getName(), cinema.getCity()}
@@ -23,8 +54,10 @@ public class GetCinemaIDView extends CommandView {
 
             header.close();
             header.writeToFile();
+            */
 
-            infoString = header.getBuildedString();
+
+
         }
     }
 }
