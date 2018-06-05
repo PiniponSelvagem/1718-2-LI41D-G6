8256eb1cc863547582c45b00685ea9ced7ad01f5
Twigz57 <41457@alunos.isel.ipl.pt>
2018-06-05 12:27:16 +0100

    PostGreSQL first try enabling (somos nabos)

--- src/main/java/pt/isel/ls/sql/Sql.java
+++ src/main/java/pt/isel/ls/sql/Sql.java
@@ -3,12 +3,31 @@ package pt.isel.ls.sql;
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 
-import java.sql.Connection;
+import java.sql.*;
 
 /**
  * Created by Nuno on 27/02/2018.
  */
 public class Sql {
+    private static Connection con;
+    private static void CreateConnetion() throws SQLServerException {
+        String url = "jdbc:postgresql://localhost:5432/"+System.getenv("LS-server");
+        String user = System.getenv("LS-user");
+        String password = System.getenv("LS-pass");
+        try {
+            con = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection() throws SQLServerException {
+        CreateConnetion();
+        return con;
+    }
+}
+
+/*
+* public class Sql {
     private static SQLServerDataSource ds = null;
     private static void CreateConnetion() throws SQLServerException {
         ds = new SQLServerDataSource();
@@ -30,4 +49,4 @@ public class Sql {
         return ds.getConnection();
     }
 }
-
+*/
